a18cbd2259de21d975d4499ee9d0f50f87438735
saideekshith8 <saideekshith@zibtek.in>
2022-04-07 17:33:52 +0530

    "Registration Test Script"

---
+++ CanonizerRegistrationPage.py
@@ -0,0 +1,133 @@
+import time
+from time import sleep
+
+from CanonizerBase import Page
+from Identifiers import RegisterPageIdentifiers
+
+
+class CanonizerRegisterPage(Page):
+    """
+        Class Name: CanonizerRegisterPage
+
+        """
+
+    def click_register_button(self):
+        """
+        -> Hover the control towards the register button. Identifiers are loaded from Identifiers Class
+        -> Find the register button and Click on it.
+
+        :return:
+            Return the control to the main Program
+        """
+
+        self.hover(*RegisterPageIdentifiers.CLICK_REGISTER_BUTTON)
+        self.find_element(*RegisterPageIdentifiers.CLICK_REGISTER_BUTTON).click()
+        return CanonizerRegisterPage(self.driver)
+
+    def click_register_page_and_enter_fields_with_valid_data(self, first, last, email, password, confirm_password):
+        self.click_register_button()
+        self.find_element(*RegisterPageIdentifiers.FIRST_NAME).send_keys(first)
+        self.find_element(*RegisterPageIdentifiers.LAST_NAME).send_keys(last)
+        self.find_element(*RegisterPageIdentifiers.EMAIL).send_keys(email)
+        self.find_element(*RegisterPageIdentifiers.PASSWORD).send_keys(password)
+        self.find_element(*RegisterPageIdentifiers.CONFIRM_PASSWORD).send_keys(confirm_password)
+        sleep(3)
+        self.find_element(*RegisterPageIdentifiers.REGISTER_NOW).click()
+        return CanonizerRegisterPage(self.driver)
+
+    def click_on_close_icon_register_page(self):
+        self.click_register_button()
+        sleep(3)
+        self.hover(*RegisterPageIdentifiers.CLOSE_ICON)
+        self.find_element(*RegisterPageIdentifiers.CLOSE_ICON).click()
+        return CanonizerRegisterPage(self.driver)
+
+    def craete_canonizer_register_page_without_entering_data(self, first, last, email, password, confirm_password):
+        self.click_register_button()
+        self.find_element(*RegisterPageIdentifiers.FIRST_NAME).send_keys(first)
+        self.find_element(*RegisterPageIdentifiers.LAST_NAME).send_keys(last)
+        self.find_element(*RegisterPageIdentifiers.EMAIL).send_keys(email)
+        self.find_element(*RegisterPageIdentifiers.PASSWORD).send_keys(password)
+        self.find_element(*RegisterPageIdentifiers.CONFIRM_PASSWORD).send_keys(confirm_password)
+        self.find_element(*RegisterPageIdentifiers.REGISTER_NOW).click()
+        return CanonizerRegisterPage(self.driver)
+
+    def create_canonizer_register_page_entering_with_invalid_data(self, first, last, email, password, confirm_password):
+        self.click_register_button()
+        self.find_element(*RegisterPageIdentifiers.FIRST_NAME).send_keys(first)
+        self.find_element(*RegisterPageIdentifiers.LAST_NAME).send_keys(last)
+        self.find_element(*RegisterPageIdentifiers.EMAIL).send_keys(email)
+        self.find_element(*RegisterPageIdentifiers.PASSWORD).send_keys(password)
+        self.find_element(*RegisterPageIdentifiers.CONFIRM_PASSWORD).send_keys(confirm_password)
+        self.find_element(*RegisterPageIdentifiers.REGISTER_NOW).click()
+        return CanonizerRegisterPage(self.driver)
+
+    def create_canonizer_page_with_existed_data(self, first, last, email, password, confirm_password):
+        self.click_register_button()
+        self.find_element(*RegisterPageIdentifiers.FIRST_NAME).send_keys(first)
+        self.find_element(*RegisterPageIdentifiers.LAST_NAME).send_keys(last)
+        self.find_element(*RegisterPageIdentifiers.EMAIL).send_keys(email)
+        self.find_element(*RegisterPageIdentifiers.PASSWORD).send_keys(password)
+        self.find_element(*RegisterPageIdentifiers.CONFIRM_PASSWORD).send_keys(confirm_password)
+        sleep(3)
+        self.hover(*RegisterPageIdentifiers.REGISTER_NOW)
+        self.find_element(*RegisterPageIdentifiers.REGISTER_NOW).click()
+        sleep(4)
+        return CanonizerRegisterPage(self.driver)
+
+    def create_registration_page_without_entering_in_one_mandatary_field(self, first, email, password,
+                                                                         confirm_password):
+        self.click_register_button()
+        self.find_element(*RegisterPageIdentifiers.FIRST_NAME).send_keys(first)
+        self.find_element(*RegisterPageIdentifiers.EMAIL).send_keys(email)
+        self.find_element(*RegisterPageIdentifiers.PASSWORD).send_keys(password)
+        self.find_element(*RegisterPageIdentifiers.CONFIRM_PASSWORD).send_keys(confirm_password)
+        self.find_element(*RegisterPageIdentifiers.REGISTER_NOW).click()
+        return CanonizerRegisterPage(self.driver)
+
+    def registration_title_identification(self):
+        self.hover(*RegisterPageIdentifiers.TITLE)
+        title = self.find_element(*RegisterPageIdentifiers.TITLE).text
+        print(title)
+        if title == 'Register Now on Canonizer':
+            return CanonizerRegisterPage(self.driver)
+
+    def verify_firstname_empty(self, Name):
+        self.click_register_button()
+        self.hover(*RegisterPageIdentifiers.FNAME_HEADING)
+        title = self.find_element(*RegisterPageIdentifiers.FNAME).text
+        print(title)
+        self.find_element(*RegisterPageIdentifiers.FIRST_NAME).send_keys(Name)
+        self.find_element(*RegisterPageIdentifiers.LAST_NAME).click()
+        # self.find_element(*RegistrationPageIdentifiers.Register_now).click()
+        # self.hover(*RegistrationPageIdentifiers.error)
+        err = self.find_element(*RegisterPageIdentifiers.error).text
+        print(err)
+        if err == 'Please input your first name!':
+            return CanonizerRegisterPage(self.driver)
+
+    def verify_all_fields_empty(self):
+        self.click_register_button()
+        sleep(2)
+        self.find_element(*RegisterPageIdentifiers.Register_now).click()
+        self.hover(*RegisterPageIdentifiers.error)
+        err = self.find_element(*RegisterPageIdentifiers.error).text
+        print(err)
+        if err == 'Please input your first name!':
+            return CanonizerRegisterPage(self.driver)
+
+    def verify_invalid_password_format(self,fname,lname,email,password):
+        self.click_register_button()
+        self.hover(*RegisterPageIdentifiers.FNAME_HEADING)
+        self.find_element(*RegisterPageIdentifiers.FIRST_NAME).send_keys(fname)
+        self.find_element(*RegisterPageIdentifiers.LAST_NAME).send_keys(lname)
+        self.find_element(*RegisterPageIdentifiers.EMAIL).send_keys(email)
+        self.find_element(*RegisterPageIdentifiers.PASSWORD).send_keys(password)
+        self.find_element(*RegisterPageIdentifiers.Register_now).click()
+        time.sleep(2)
+        # self.hover(*RegisterPageIdentifiers.INVALID_PASSWORD_ERROR)
+        err = self.find_element(*RegisterPageIdentifiers.INVALID_PASSWORD_ERROR).text
+        print("1234----")
+        print(err)
+        if err == 'Password must be contain small, capital letter, number and special character like Abc@1234.':
+            return CanonizerRegisterPage(self.driver)
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import unittest
+
+from selenium.webdriver.remote.webelement import WebElement
+
 from CanonizerHomePage import *
+from CanonizerRegistrationPage import CanonizerRegisterPage
 
 from CanonizerTestCases import test_cases
 from Config import *
 from selenium import webdriver
+
 
 class TestPages(unittest.TestCase):
 
@@ -26,6 +31,76 @@ class TestPages(unittest.TestCase):
     def test_canonizer_home_page_load(self):
         print("\n" + str(test_cases(0)))
         self.assertTrue(CanonizerMainPage(self.driver).check_home_page_loaded())
+
+    # 02
+    def test_canonizer_click_register_page(self):
+        result = CanonizerRegisterPage(self.driver).click_register_button().get_url()
+        self.assertIn("", result)
+
+    # 03
+    def test_click_register_page_entering_fields_with_valid_data(self):
+        result = CanonizerRegisterPage(self.driver).click_register_page_and_enter_fields_with_valid_data('kumar',
+                                                                                                         'file',
+                                                                                                         'saideekshith@zibtek.in',
+                                                                                                         'Sai@1998',
+                                                                                                         'Sai@1998').get_url()
+        self.assertIn("", result)
+
+    # 04
+    def test_click_on_register_page_and_click_on_close_icon(self):
+        result = CanonizerRegisterPage(self.driver).click_on_close_icon_register_page().get_url()
+        self.assertIn("", result)
+
+    # 05
+    def test_create_register_page_without_entering_data(self):
+        result = CanonizerRegisterPage(self.driver).craete_canonizer_register_page_without_entering_data("", "", "", "",
+                                                                                                         "").get_url()
+        self.assertIn("//canonizer3.canonizer.com/", result)
+
+    # 06
+    def test_create_register_page_entering_invalid_data(self):
+        result = CanonizerRegisterPage(self.driver).create_canonizer_register_page_entering_with_invalid_data(
+            'kumar_sai', 'file    1', 'saideekshith@zibtek.in', 'Sai@1998', 'Sai@1998').get_url()
+        self.assertIn("", result)
+
+    # 07
+    def test_create_registration_page_with_existed_data(self):
+        result = CanonizerRegisterPage(self.driver).create_canonizer_page_with_existed_data('kumar', 'file',
+                                                                                            'saideekshith@zibtek.in',
+                                                                                            'Sai@1998',
+                                                                                            'Sai@1998').get_url()
+        self.assertIn("", result)
+
+    # 08
+    def test_create_registration_page_without_entering_in_one_mandatary_field(self):
+        result = CanonizerRegisterPage(self.driver).create_registration_page_without_entering_in_one_mandatary_field(
+            'kumar', 'saideekshith@zibtek.in', 'Sai@1998', 'Sai@1998').get_url()
+        self.assertIn("", result)
+
+    # 09
+    def test_registration_title_identification(self):
+        print("\n" + str(test_cases(4)))
+        result = CanonizerRegisterPage(self.driver).registration_title_identification().get_url()
+        print(result)
+
+    # 10
+    def test_verify_firstname_empty(self):
+        print("\n" + str(test_cases(5)))
+        result = CanonizerRegisterPage(self.driver).verify_firstname_empty('').get_url()
+        print(result)
+
+    # 11
+    def test_verify_all_fields_empty(self):
+        print("\n" + str(test_cases(6)))
+        result = CanonizerRegisterPage(self.driver).verify_all_fields_empty().get_url()
+        print(result)
+
+    # 12
+    def test_verify_invalid_password_format(self):
+        result = CanonizerRegisterPage(self.driver).verify_invalid_password_format("A","bn","son12@zibtek.in","Sonali@zibtek.in",).get_url()
+        print(result)
+
+    # 13
 
 
 
