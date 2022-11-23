c3d7a77836055f5ecd49138feec4cc7068552592
akashgit77 <105647543+akashgit77@users.noreply.github.com>
2022-11-23 18:04:56 +0530

    Update CanonizerSupportValue.py

--- CanonizerSupportValue.py
+++ CanonizerSupportValue.py
@@ -107,72 +107,179 @@ class CanonizerSupportValue(Page):
         action.key_down(Keys.ENTER).perform()
         #self.driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div[2]/div[1]/div/div/div[2]/span").click()
         #self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div/div/div/div/div[2]/form/button[1]/span").click()
-        self.driver.find_element(By.ID, "create-topic-btn").click()                
-        #Topic Created
+        self.driver.find_element(By.ID, "create-topic-btn").click()
+
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
+        #wait untill loader is present
         # Create Camp
+        #Click on Plus Icon
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div/span[2]/span").click()
-        time.sleep(3)
-        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[2]/span[3]/span").click()
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
+        #CLicking on Start CAmp Here
+        self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[2]/span[3]/span").click()
+        print("Creating Camp")
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
         self.camp = ("camp_new1" + self.n)
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/div/div/div/div/div[2]/form/div[2]/div[1]/div/div[2]/div[1]/div/input").send_keys(self.camp)
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/div/div/div/div/div[2]/form/div[2]/div[2]/div/div[2]/div/div/input").send_keys(self.camp)
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/div/div/div/div/div[2]/form/div[2]/div[3]/div/div[2]/div/div/textarea").send_keys(self.camp)
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/div/div/div/div/div[2]/form/div[2]/div[4]/div/div[2]/div/div/input").send_keys("https://www.google.com")
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.ID, "camp-about-nick-dropdown").click()
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.ID, "camp-about-nick-301").click()
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.ID, "crate-camp-btn").click()
-        time.sleep(10)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head-wrapper')))
+        except TimeoutException:
+            pass
+
+        print("Creating Second Camp")
+
+
         # Create second camp
         self.n = random.randint(0, 10000)
         self.n = str(self.n)
         self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[1]/span[3]/span/div").click()
         # self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[2]/span[3]/span").click()
-        time.sleep(3)
-        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[2]/span[3]/span").click()
-        time.sleep(3)
+        try:
+            #WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-collapse ant-collapse-icon-position-end topicDetailsCollapse')))
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card-head')))
+        except TimeoutException:
+            pass
+
+
+        #Testing is here
+        #Clicking on Start New Camp
+        print("Clicking on start new camp")
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[2]/span[3]").click()
+        print("Clicked on start new camp")
+        time.sleep(5)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card ant-card-bordered can-card-style createNewTopic_form_card__nUKJV')))
+        except TimeoutException:
+            pass
+        print("Checked condition")
         self.camp = ("camp_new1" + self.n)
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/div/div/div/div/div[2]/form/div[2]/div[1]/div/div[2]/div[1]/div/input").send_keys(self.camp)
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card ant-card-bordered can-card-style createNewTopic_form_card__nUKJV')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/div/div/div/div/div[2]/form/div[2]/div[2]/div/div[2]/div/div/input").send_keys(self.camp)
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card ant-card-bordered can-card-style createNewTopic_form_card__nUKJV')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/div/div/div/div/div[2]/form/div[2]/div[3]/div/div[2]/div/div/textarea").send_keys(self.camp)
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card ant-card-bordered can-card-style createNewTopic_form_card__nUKJV')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/div/div/div/div/div[2]/form/div[2]/div[4]/div/div[2]/div/div/input").send_keys("https://www.google.com")
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card ant-card-bordered can-card-style createNewTopic_form_card__nUKJV')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.ID, "camp-about-nick-dropdown").click()
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card ant-card-bordered can-card-style createNewTopic_form_card__nUKJV')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.ID, "camp-about-nick-301").click()
-        time.sleep(3)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-card ant-card-bordered can-card-style createNewTopic_form_card__nUKJV')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.ID, "crate-camp-btn").click()
-        time.sleep(10)
+
+
+
+        print("Tessting is here")
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-spin-dot-item')))
+        except TimeoutException:
+            pass
         # Support first camp
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[5]/div/div/div/div[2]/div/div[2]/a/button/span").click()
-        time.sleep(5)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-spin-dot-item')))
+        except TimeoutException:
+            pass
         self.driver.find_element(By.ID, "uploadBtn").click()
-        time.sleep(10)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-spin-dot-item')))
+        except TimeoutException:
+            pass
         # Support second camp
         # self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[3]/span[3]/span/div").click()
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[3]/span[3]/span/div/div/span[1]").click()
-        time.sleep(5)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-spin-dot-item')))
+        except TimeoutException:
+            pass
+        print("Clicking on second camp")
         # self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[5]/div/div/div/div[2]/div/div[2]/a/button/span").click()
         self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div/div[1]/div[2]/div[5]/div/div/div/div[2]/div/div[2]/a/button").click()
-        time.sleep(5)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-spin-dot-item')))
+        except TimeoutException:
+            pass
+        print("Clicking on manage and support")
         self.driver.find_element(By.ID, "uploadBtn").click()
-        time.sleep(10)
+        print("Clicking on submit button")
+        try:
+            WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ant-spin-dot-item')))
+        except TimeoutException:
+            pass
         self.topic_score = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[1]/span[3]/span/div/div/span[2]")
+        print(self.topic_score.text)
         self.topic_score = float(self.topic_score.text)
+        '''self.topic_value = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[1]/span[3]/span/div")
+        print(self.topic_value.text)
+        self.topic_value = float(self.topic_value.text)
+        print(self.topic_value)'''
         self.camp1_score = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[2]/span[3]/span/div/div/span[2]")
+        print(self.camp1_score.text)
         self.camp1_score = float(self.camp1_score.text)
+        print(self.camp1_score)
         self.camp2_score = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div[3]/div/div/div/div[3]/span[3]/span/div/div/span[2]")
+        print(self.camp2_score.text)
         self.camp2_score = float(self.camp2_score.text)
+        print(self.camp2_score)
         self.camp_sum = self.camp1_score + self.camp2_score
         self.camp_sum = float(self.camp_sum)
         self.camp_sum = ("%.2f" % self.camp_sum)
+        print(self.camp_sum)
         return CanonizerSupportValue(self.driver)
