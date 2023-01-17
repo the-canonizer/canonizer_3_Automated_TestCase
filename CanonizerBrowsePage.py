821fcc24287afeab916fc1e62ffbe4625327f6c2
akashgit77 <105647543+akashgit77@users.noreply.github.com>
2023-01-17 12:15:00 +0530

    Update CanonizerBrowsePage.py

--- CanonizerBrowsePage.py
+++ CanonizerBrowsePage.py
@@ -1084,22 +1084,6 @@ class CanonizerBrowsePage(Page):
         self.click_browse_page_button()
         self.select_dropdown_value()
         self.scroll_namespaces()
+        if self.current_name_list==self.namespaces_list:
+           return True
         return CanonizerBrowsePage(self.driver)
-     def select_all_namespaces(self):
-        self.click_browse_page_button()
-        self.select_dropdown_value()
-        self.driver.implicitly_wait(10)
-        action = ActionChains(driver)
-        n = 40
-        i = 1
-        while n >= 0:
-            action.key_down(Keys.DOWN).perform()
-            action.key_down(Keys.ENTER).perform()
-            n = n - 1
-            self.driver.implicitly_wait(10)
-            # Check
-            self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div/div/div/div/div[1]/div/div/div[1]/div/label/span[1]/input").click()
-            # Uncheck
-            self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div/div/div/div/div[1]/div/div/div[1]/div/label/span[1]/input").click()
-            self.select_dropdown_value()
-
