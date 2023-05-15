6a32eee5a9f0e6f2306ee12f8462bef49fa93a9f
akashgit77 <105647543+akashgit77@users.noreply.github.com>
2023-05-15 16:30:43 +0530

    Update Canonizer_Url_Redirection.py

--- Canonizer_Url_Redirection.py
+++ Canonizer_Url_Redirection.py
@@ -42,17 +42,13 @@ class CanonizerUrlRedirection(Page):
         self.n = random.randint(0, 10000)
         self.n = str(self.n)
         self.driver.get("http://canonizer3.canonizer.com/topic.asp/" + self.n)
-        time.sleep(5)
 
         resp = self.driver.current_url
         reslt = ("/topic/" + self.n)
-        print(resp)
-        print(reslt)
         if resp == reslt:
             pass
         else:
             mailer.mailern()
-            print("failed")
         return CanonizerUrlRedirection(self.driver)
 
 
@@ -61,18 +57,14 @@ class CanonizerUrlRedirection(Page):
 
         res = requests.get("https://canonizer.com/videos/consciousness?chapter=representational_qualia_consensus")
         res = str(res.status_code)
-        print(res)
         if "200" == res:
             url = ("https://" + "canonizer3" + ".canonizer.com/videos/consciousness?chapter=representational_qualia_consensus")
             self.driver.get(url)
             self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div[1]/div/div/label[1]/span[1]/input").click()
-            time.sleep(10)
 
             self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[2]/div[2]/video").click()
             if self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[2]/div[2]/video"):
                 self.resp = "passed"
-                # resp = self.driver.current_url
-                print(self.resp)
                 return CanonizerUrlRedirection(self.driver)
 
         else:
@@ -83,7 +75,6 @@ class CanonizerUrlRedirection(Page):
         self.driver.implicitly_wait(30)
 
         self.driver.get("http://canonizer3.canonizer.com/support_list.asp?nick_name_id=1")
-        time.sleep(30)
         if self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[2]/div[1]/div[2]/div/div[1]/div/div[2]/h3"):
             self.status = "200"
             return CanonizerUrlRedirection(self.driver)
@@ -95,7 +86,6 @@ class CanonizerUrlRedirection(Page):
         self.driver.implicitly_wait(30)
 
         self.driver.get("http://canonizer3.canonizer.com/thread.asp/23/13/4")
-        time.sleep(20)
         if self.driver.find_element(By.ID, "create-btn"):
             self.status = "200"
             return CanonizerUrlRedirection(self.driver)
@@ -107,7 +97,6 @@ class CanonizerUrlRedirection(Page):
         self.driver.implicitly_wait(30)
 
         self.driver.get("http://canonizer3.canonizer.com/forum.asp/88/1")
-        time.sleep(20)
         if self.driver.find_element(By.ID, "create-btn"):
             self.status = "200"
             return CanonizerUrlRedirection(self.driver)
@@ -119,7 +108,6 @@ class CanonizerUrlRedirection(Page):
         self.driver.implicitly_wait(30)
 
         self.driver.get("http://canonizer3.canonizer.com/topoc.asp/85")
-        time.sleep(20)
         if self.driver.find_element(By.ID, "camp-forum-btn"):
             self.status = "200"
             return CanonizerUrlRedirection(self.driver)
@@ -130,7 +118,6 @@ class CanonizerUrlRedirection(Page):
     def manage_asp_url_redirection(self):
         self.driver.implicitly_wait(30)
         self.driver.get("http://canonizer3.canonizer.com/manage.asp/2/2?class=camp")
-        time.sleep(20)
         if self.driver.find_element(By.ID, "create-topic-btn"):
             self.status = "200"
             return CanonizerUrlRedirection(self.driver)
@@ -142,7 +129,6 @@ class CanonizerUrlRedirection(Page):
     def statement_asp_url_redirection(self):
         self.driver.implicitly_wait(30)
         self.driver.get("http://canonizer3.canonizer.com/statement.asp/2/2")
-        #time.sleep(20)
         if self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/button[1]/span"):
             self.status = "200"
             return CanonizerUrlRedirection(self.driver)
@@ -154,7 +140,6 @@ class CanonizerUrlRedirection(Page):
     def stmt_asp_url_redirection(self):
         self.driver.implicitly_wait(30)
         self.driver.get("http://canonizer3.canonizer.com/stmt.asp/2/2")
-        #time.sleep(20)
         if self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/button[1]/span"):
             self.status = "200"
             return CanonizerUrlRedirection(self.driver)
@@ -170,10 +155,8 @@ class CanonizerUrlRedirection(Page):
             # if json response have data = true parameter then redirect else return 404.
             self.driver.get("https://canonizer3.canonizer.com/sitemap.xml")
             self.resf = requests.get("https://canonizer3.canonizer.com/sitemap.xml").text
-            time.sleep(5)
             from bs4 import BeautifulSoup
             soup = BeautifulSoup(self.resf, "html.parser")
-            print(soup.h1.text)
             return CanonizerUrlRedirection(self.driver)
 
         else:
