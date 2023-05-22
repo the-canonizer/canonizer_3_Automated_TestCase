0781d4e3e9b2cea3de5ec34c948aac608839c962
akashgit77 <105647543+akashgit77@users.noreply.github.com>
2023-05-22 12:13:29 +0530

    Update CanonizerFooter.py

--- CanonizerFooter.py
+++ CanonizerFooter.py
@@ -17,32 +17,38 @@ class CanonizerFooter(Page):
         self.driver.implicitly_wait(30)
         self.hover(*CanonizerFooterIdentifiers.FOOTER_BROWSE)
         self.driver.find_element(*CanonizerFooterIdentifiers.FOOTER_BROWSE).click()
+        WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, "topicsList_head__IooBi topicsList_browsePage__cb2G_")))
 
     def footer_create_new_topic(self):
         self.driver.implicitly_wait(30)
         self.hover(*CanonizerFooterIdentifiers.FOOTER_NEW_TOPIC)
         self.driver.find_element(*CanonizerFooterIdentifiers.FOOTER_NEW_TOPIC).click()
+        WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, "topicsList_head__IooBi topicsList_browsePage__cb2G_")))
 
     def footer_upload(self):
         self.driver.implicitly_wait(30)
         self.hover(*CanonizerFooterIdentifiers.FOOTER_UPLOAD)
         self.driver.find_element(*CanonizerFooterIdentifiers.FOOTER_UPLOAD).click()
+        WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, "topicsList_head__IooBi topicsList_browsePage__cb2G_")))
 
     def footer_sitemap(self):
         self.driver.implicitly_wait(30)
         self.hover(*CanonizerFooterIdentifiers.FOOTER_SITEMAP)
         self.driver.find_element(*CanonizerFooterIdentifiers.FOOTER_SITEMAP).click()
+        WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, "topicsList_head__IooBi topicsList_browsePage__cb2G_")))
 
     def footer_help(self):
         self.driver.implicitly_wait(30)
         self.hover(*CanonizerFooterIdentifiers.FOOTER_HELP)
         self.driver.find_element(*CanonizerFooterIdentifiers.FOOTER_HELP).click()
+        WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, "topicsList_head__IooBi topicsList_browsePage__cb2G_")))
 
 
     def footer_white_paper(self):
         self.driver.implicitly_wait(30)
         self.hover(*CanonizerFooterIdentifiers.FOOTER_WHITEPAPER)
         self.driver.find_element(*CanonizerFooterIdentifiers.FOOTER_WHITEPAPER).click()
+        WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, "topicsList_head__IooBi topicsList_browsePage__cb2G_")))
 
 
 
@@ -50,9 +56,11 @@ class CanonizerFooter(Page):
         self.driver.implicitly_wait(30)
         self.hover(*CanonizerFooterIdentifiers.FOOTER_BLOG)
         self.driver.find_element(*CanonizerFooterIdentifiers.FOOTER_BLOG).click()
+        WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, "topicsList_head__IooBi topicsList_browsePage__cb2G_")))
 
 
     def footer_jobs(self):
         self.driver.implicitly_wait(30)
         self.hover(*CanonizerFooterIdentifiers.FOOTER_JOBS)
         self.driver.find_element(*CanonizerFooterIdentifiers.FOOTER_JOBS).click()
+        WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, "topicsList_head__IooBi topicsList_browsePage__cb2G_")))
